5c430ca505a5589e0a35b6f61d937bd0526881b7
victorsaturno <victorsaturno@disroot.org>
2020-07-14 03:23:57 -0300

    Se agregarón comentarios en métodos

--- Ficha.cpp
+++ Ficha.cpp
@@ -4,10 +4,12 @@ Ficha::Ficha() {
 	valor = '-';
 }
 
+//método que le carga un valor a la ficha
 void Ficha::setValor(char valor) {
 	this->valor = valor;
 }
 
+//método que devuelve el valor de la ficha
 char Ficha::getValor(){
 	return valor;
 }
